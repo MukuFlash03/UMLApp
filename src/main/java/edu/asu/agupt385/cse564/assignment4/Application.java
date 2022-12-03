91090ca46ad7f3fc54f028939b3296bb27771439
Anand Gupta <anandg@asu.edu>
2022-12-02 20:07:15 -0700

    Remove comments

--- src/main/java/edu/asu/agupt385/cse564/assignment4/Application.java
+++ src/main/java/edu/asu/agupt385/cse564/assignment4/Application.java
@@ -1,24 +1,13 @@
 package edu.asu.agupt385.cse564.assignment4;
 
-import edu.asu.agupt385.cse564.assignment4.util.GraphImporter;
 import edu.asu.agupt385.cse564.assignment4.view.ApplicationGUI;
 
 import javax.swing.SwingUtilities;
 
 public class Application {
     public static void main(String[] args) {
-        GraphImporter importer = new GraphImporter();
-//        importer.importGraph(Application.class.getClassLoader().getResourceAsStream("graph.dot"));
         SwingUtilities.invokeLater(() -> {
             new ApplicationGUI("Assignment 4_5");
         });
-//        GraphDataSource dataSource = GraphDataSource.getInstance();
-//        dataSource.getAllVertices().stream().forEach(vertex -> {
-//            System.out.println(vertex.getName());
-//            System.out.println(vertex.getX() + ", " + vertex.getY());
-//        });
-//        dataSource.getAllEdges().stream().forEach(edge -> {
-//            System.out.println(edge.getSource().getName() + " -- " + edge.getDestination().getName() + " " + edge.getRelationshipType());
-//        });
     }
 }
